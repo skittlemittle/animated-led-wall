5605f2af2c760802db4d0a52b086e51beb253cdb
cautiouspotato <afshaanlondhe4@gmail.com>
2020-08-26 18:09:45 +0530

    broken now innit
    pushing so i can completely rewrite the whole recieving bit
    
    its broken now innit
    
    push before rewrite

--- arduino/bruh/bruh.ino
+++ arduino/bruh/bruh.ino
@@ -5,39 +5,21 @@
 int gndPins[] = {5, 6, 7};
 int pwrPins[] = {8, 9, 10};
 
-const int len_sequence = 3;
-int* sequence[len_sequence];
+const int len_sequence = 2;
+int* sequence[50];
+int frameRate = 10;
+bool sequenceExists = false;
 
 void setup()
 {
     Serial.begin(9600);
-    Serial.println(sizeof sequence);
     for (int i = 0; i < sizeof gndPins / sizeof gndPins[0]; i++) {
         pinMode(gndPins[i], OUTPUT);
         pinMode(pwrPins[i], OUTPUT);
         // have em all off
         digitalWrite(gndPins[i], HIGH);
     }
-    
-    // generate sequence of deltas
-    for (int i = 0; i < len_sequence; i++) {
-        sequence[i] = new int[10];  /// new int[frameSize];
-        // delta frames
-        for (int j = 0; j < 10; j+=5) {
-            /*
-            sequence[i][j]   = 3; // row
-            sequence[i][j+1] = 2; // col
-            sequence[i][j+2] = 255; // r
-            sequence[i][j+3] = 0; // g
-            sequence[i][j+4] = 0; // b
-            Serial.print(i);
-            Serial.print("__");
-            Serial.print(j);
-            Serial.println("AAAAAA");
-            */
-        }
-    }
-
+/*
     // dummy deltas
     //1
     sequence[0][0] = 0;
@@ -46,7 +28,7 @@ void setup()
     sequence[0][3] = 0;
     sequence[0][4] = 0;
     //
-    sequence[0][5] = 2;
+    sequence[0][5] = 1; //2
     sequence[0][6] = 0;
     sequence[0][7] = 0;
     sequence[0][8] = 0;
@@ -75,16 +57,16 @@ void setup()
     sequence[2][7] = 255;
     sequence[2][8] = 0;
     sequence[2][9] = 0;
+*/
 }
 
-// AAAAAAAA GHOSTING
 void loop()
 {
+    if (!sequenceExists) return;
+
     int curFrame[3][3][3] = {0};
     // generate the frame amirite
     for (int frame = 0; frame < len_sequence; frame++) {
-        Serial.print("--------");
-        Serial.println(frame);
         // TODO: magic numbers!
         for (int j = 0; j < 10; j+=5) {
             // monkey brain
@@ -101,7 +83,6 @@ void loop()
         // draw the frame
         for (int i = 0; i < sizeof gndPins / sizeof gndPins[0]; i++) {
             for (int j = 0; j < sizeof gndPins / sizeof gndPins[0]; j++) {
-                Serial.println(curFrame[i][j][0]);
                 if (curFrame[i][j][0] > 0) {
                     digitalWrite(gndPins[i], LOW);
                     analogWrite(pwrPins[j], curFrame[i][j][0]);
@@ -109,9 +90,79 @@ void loop()
                 }
             }
         }
-        delay(1000);
+        delay(1000 / frameRate);
         for (size_t i = 0; i < sizeof gndPins / sizeof gndPins[0]; i++) {
             digitalWrite(gndPins[i], HIGH);
         }
     }
+}
+
+/*
+ * Read the animation
+ */
+void serialEvent()
+{
+    int frameCount = 0;
+    int valCount = 0;
+    bool headerRead = false;
+    // me and the boys parsing
+    while (Serial.available()) {
+        char check = (char)Serial.peek();
+        size_t didRead = 0;
+        char value[3] = "";
+
+        if (headerRead) {
+            if (check == '+') {        // new frame
+                frameCount++;
+                Serial.read();
+            } else if (check == ',') { // new val
+                valCount++;
+                Serial.read();
+            } else {
+                didRead = Serial.readBytes(value, 3);
+            }
+            // finally write it
+            if (didRead > 0) {
+                sequence[frameCount][valCount] = atoi(value);
+                Serial.println(atoi(value));
+            }
+        } else {
+            /*
+             * Read header and prep sequence array
+             * numFrames, frameRate, and each frameSize are all 2 bytes
+             */
+            char nfArr[4];
+            Serial.readBytes(nfArr, 4);
+            String head = String(nfArr);
+            int numFrames = head.substring(0,2).toInt();
+            frameRate = head.substring(2).toInt();
+            Serial.print("Header");
+            Serial.print(numFrames);
+            Serial.print(' ');
+            Serial.println(frameRate);
+
+            for (size_t i = 0; i < numFrames; i++) {
+                char frameSizeArr[2] = "";
+                Serial.readBytes(frameSizeArr, 2);
+                const int frameSize = atoi(frameSizeArr);
+                sequence[i] = new int[frameSize];
+            }
+            headerRead = true;
+        }
+    }
+
+    for (int i = 0; i < len_sequence; i++) {
+        for (int j = 0; j < 10; j += 5) {
+            Serial.print(sequence[i][j]);
+            Serial.print(' ');
+            Serial.print(sequence[i][j+1]);
+            Serial.print(' ');
+            Serial.print(sequence[i][j+2]);
+            Serial.print(' ');
+            Serial.print(sequence[i][j+3]);
+            Serial.print(' ');
+            Serial.println(sequence[i][j+4]);
+        }
+    }
+    sequenceExists = true;
 }
